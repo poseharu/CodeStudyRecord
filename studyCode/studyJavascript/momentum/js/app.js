ea7956ba0c811f7ed797ed26ea25121aad40ae10
poseharu <poseharu@gmail.com>
2022-03-08 23:24:21 +0900

    사이트명 수정

--- studyCode/studyJavascript/momentum/js/app.js
+++ studyCode/studyJavascript/momentum/js/app.js
@@ -7,7 +7,7 @@ window.addEventListener('DOMContentLoaded', function(){
   initLogInOut();
   initMusic();
   initTodayInfo();
-  // initWeather();
+  initWeather();
   initTodo();
 
   commonEventListener();
